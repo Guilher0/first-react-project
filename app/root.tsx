1d841281c15dcf6c7467e8b49856f3aaad525325
Guilher0 <guilherme.bs10@gmail.com>
2024-03-15 17:48:03 -0300

    Initial commit from create-remix

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,4 @@
+import { LinksFunction } from "@remix-run/node";
 import {
   Links,
   Meta,
@@ -6,16 +7,23 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+import styles from "app/styles/styles.css"; 
+
+export const links: LinksFunction = () => {
+
+  return [{rel:"stylesheet", href: styles}];
+}
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="pt">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
-      <body>
+      <body style={{margin:"0"}}>
         {children}
         <ScrollRestoration />
         <Scripts />
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,33 @@
 import type { MetaFunction } from "@remix-run/node";
 
+
+
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "React" },
+    { name: "description", content: "Welcome to React!" },
   ];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+  <div className="main">
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.6", padding:"35px 0", background: "rgb(114, 166, 181)", textAlign:"center"}}>
+      <img src="img/react.png" alt="React Remix" title="react remix"/>
+      <h1>Welcome to  React Remix</h1>
+      
+      <p>A biblioteca para interfaces de usuários Web e Nativas</p>
     </div>
+
+    <div style={{padding:"35px 0", background: "rgb(114, 166, 175)", textAlign:"center", marginTop:"10px"}} >
+      <form action="" method="post" style={{display:"flex",marginInline:"auto" ,justifyContent:"space-between", maxWidth:"300px"}}>
+      <input type="button" value="Aprenda React" />
+      <input type="button" value="Referencia da API" />
+      </form>
+    </div>
+
+
+  </div>
   );
 }
+
